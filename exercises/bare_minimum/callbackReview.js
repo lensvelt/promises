85465bb364bbd6c4b681e4ed60729ef29ed3507e
Brett <brett@applogix.com>
2016-05-12 20:52:46 -0700

    Pass first test of callbackReview spec

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,8 +6,13 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, function(err, data) {
+    if (!err) {
+      var firstLine = data.toString().split('\n')[0];
+      callback(null, firstLine);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
