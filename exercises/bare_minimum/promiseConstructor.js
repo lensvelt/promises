ab1ff324344f1b13b342c5ceb6bf86354346afd8
Brett <brett@applogix.com>
2016-05-12 21:59:26 -0700

    Pass Promises test for readFirstLineFromFile

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -10,7 +10,15 @@ var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filePath, function(err, res) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res.toString().split('\n')[0]);
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
